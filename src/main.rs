e818b95b7bfe52936e798f9dea55704ad5c25653
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-22 01:16:53 -0400

    Creación de main.rs y Cargo.lock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
+fn main() {
+  // Days of the week...
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let first_day = days[0];
+    let second_day = days[1];
+    let third_day = days[2];
+
+    println!("El 1er día de la semana es: {}", first_day);
+    println!("El 2do día de la semana es: {}", second_day);
+    println!("El 3er día de la semana es: {}", third_day);
+}
