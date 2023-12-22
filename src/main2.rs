6803e2f65f620bf8513186000acebb26659e31f4
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-22 14:40:11 -0400

    Creación de main2.rs

---
+++ src/main2.rs
@@ -0,0 +1,11 @@
+// Creación de un vector con inicialización de 3 valores y otro
+// con longitud fija asiganada...
+fn main() {
+    // Se declara un vector, se inicializa con tres valores...
+        let three_nums = vec![7, 43, 21];
+    // Se declara un vector, valor: 0 y longitud: 5,,,
+        let zeroes = vec![0; 5];
+    
+        println!("Initial vector: {:?}", three_nums);
+        println!("Zeroes: {:?}", zeroes);
+}
